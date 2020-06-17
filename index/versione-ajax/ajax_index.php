253fcedc6a11ed477b535224d0e3f57f39c020f1
Paolo <paolo.suero@gmail.com>
2020-06-17 18:02:16 +0200

    Terminata seconda parte

--- index/versione-ajax/ajax_index.php
+++ index/versione-ajax/ajax_index.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>Dischi musicali</title>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="../../public/app.css">
     </head>
     <body>
@@ -13,18 +14,32 @@
                     <img src="../../public/img/logo.png" alt="Logo">
                 </div>
                 <div id="header-right">
-                    <select class="" name="">
+                    <select>
                         <option value="">Tutti gli artitsti</option>
                     </select>
                 </div>
             </div>
         </header>
         <main>
-            <!-- inizio sezione contenente gli album -->
-            <section id="album">
-                <div class="container"></div>
-            </section>
+        <!-- inizio sezione contenente gli album -->
+        <section id="album">
+            <div class="container"></div>
+        </section>
         </main>
+        <!-- template di handlebars -->
+        <script id="disco-template" type="text/x-handlebars-template">
+            <div class="disco-item">
+                <div class="poster-disco">
+                    <img src="{{src}}" alt="">
+                </div>
+                <div class="info-disco">
+                    <h2>{{titolo}}</h2>
+                    <p class="author">{{autore}}</p>
+                    <p class="genre">{{genere}}</p>
+                    <p class="year">{{anno}}</p>
+                </div>
+            </div>
+        </script>
         <!-- link script JS -->
         <script src="../../public/js/versione-ajax/app.js" charset="utf-8"></script>
     </body>
--- src/js/versione-ajax/app.js
+++ src/js/versione-ajax/app.js
@@ -3,4 +3,46 @@ var $ = require( "jquery" );
 //importo la libreria di handlebars
 const Handlebars = require("handlebars");
 
-$(document).reasy
+$(document).ready(function() {
+
+    //preparo il template per il disco
+    var template_html = $("#disco-template").html();
+    var template_function = Handlebars.compile(template_html);
+
+    //effettuo la chiamata ajax per recuperare i dischi
+    $.ajax({
+        "url": "../../public/database/ajax_dischi.php",
+        "method": "GET",
+        "success": function(data) {
+            //chiamo la funzione gestione_dati che si occupa di reperire le info dai dischi
+            gestione_dati(data);
+        },
+        "error": function() {
+            alert("Si è verificato un errore");
+        }
+    })
+
+    function gestione_dati(lista_dischi) {
+        //ciclo l'array per recuperare i singoli dischi
+        for (var i = 0; i < lista_dischi.length; i++) {
+            //creo una variabile contenente il disco corrente
+            var disco = lista_dischi[i];
+            //preparo l'oggetto di handlebars
+            var context = {
+                "src" : disco.poster,
+                "titolo" : disco.title,
+                "autore" : disco.author,
+                "genere": disco.genre,
+                "anno": disco.year
+            }
+            //inserisco il disco in pagina
+            aggiungi_in_pagina(context)
+        }
+    }
+
+    function aggiungi_in_pagina(placeholder) {
+        //preparo la funzione di handlebars
+        var html_finale = template_function(placeholder);
+        $("#album .container").append(html_finale);
+    }
+})
